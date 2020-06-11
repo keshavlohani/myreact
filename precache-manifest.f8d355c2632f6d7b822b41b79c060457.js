71812dea3e9c5ce2686b8822f33d18d1215bfd00
keshavlohani <keshavlohani.dhn@gmail.com>
2020-06-11 19:34:02 +0530

    Updates

---
+++ precache-manifest.f8d355c2632f6d7b822b41b79c060457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f5dcbb07df23f41e01ceb24516c0ba",
+    "url": "/keshavlohani/myreact.git/index.html"
+  },
+  {
+    "revision": "c955e1a70ebfcb2c30d7",
+    "url": "/keshavlohani/myreact.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9993f53734133bd4752b",
+    "url": "/keshavlohani/myreact.git/static/js/2.8687d476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keshavlohani/myreact.git/static/js/2.8687d476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c955e1a70ebfcb2c30d7",
+    "url": "/keshavlohani/myreact.git/static/js/main.52744686.chunk.js"
+  },
+  {
+    "revision": "ff47453400dc9a81acdf",
+    "url": "/keshavlohani/myreact.git/static/js/runtime-main.8c24aa6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keshavlohani/myreact.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/keshavlohani/myreact.git/precache-manifest.f8d355c2632f6d7b822b41b79c060457.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keshavlohani/myreact.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
